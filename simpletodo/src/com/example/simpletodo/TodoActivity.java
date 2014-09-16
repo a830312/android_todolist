a5a336b970c96da59471289a8ebb06daed4b19c9
imangela <imangela@yahoo-inc.com>
2014-09-16 15:06:49 +0800

    add Android simple todoList app

---
+++ simpletodo/src/com/example/simpletodo/TodoActivity.java
@@ -0,0 +1,81 @@
+package com.example.simpletodo;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.apache.commons.io.FileUtils;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemLongClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+public class TodoActivity extends Activity {
+	ArrayList<String> items;
+	ArrayAdapter<String> itemsAdapter;
+	ListView lvItems;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_todo);
+		lvItems = (ListView) findViewById(R.id.lvItems);
+		readItems();
+		itemsAdapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_activated_1, items);
+		lvItems.setAdapter(itemsAdapter);
+		setListViewListener();
+	}
+	
+	private void readItems() {
+		File filesDir = getFilesDir();
+		File todoFile = new File(filesDir, "todo.txt");
+		try {
+			items = new ArrayList<String>(FileUtils.readLines(todoFile));
+		} catch (IOException e) {
+			items = new ArrayList<String>();
+			e.printStackTrace();
+		}
+	}
+	
+	public void addTodoItem(View v) {
+		EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
+		itemsAdapter.add(etNewItem.getText().toString());
+		etNewItem.setText("");
+		saveItems();
+	}
+
+	private void saveItems() {
+		// TODO Auto-generated method stub
+		File filesDir = getFilesDir();
+		File todoFile = new File(filesDir, "todo.txt");
+		try {
+			FileUtils.writeLines(todoFile, items);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void setListViewListener() {
+		// TODO Auto-generated method stub
+		lvItems.setOnItemLongClickListener(new OnItemLongClickListener() {
+			@Override
+			public boolean onItemLongClick(AdapterView<?> parent, View view,
+					int position, long rowId) {
+				items.remove(position);
+				itemsAdapter.notifyDataSetChanged();
+				saveItems();
+				return true;
+			}
+		});
+	}
+	
+}
+
